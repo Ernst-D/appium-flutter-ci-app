3727ab8c5aa6d18f7c6c5b19020e5b4b26f8f8f6
Ernst-D <ernst.dzerevanko@gmail.com>
2023-09-15 14:19:49 +0400

    integrated flutter_driver

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,15 @@ import 'package:bloc/bloc.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_counter/app.dart';
 import 'package:flutter_counter/counter_observer.dart';
+import 'package:flutter_driver/driver_extension.dart';
+
+bool enableDriverExtension() => const bool.fromEnvironment('driver');
 
 void main() {
+  if (enableDriverExtension()) {
+    enableFlutterDriverExtension();
+  }
+
   Bloc.observer = const CounterObserver();
   runApp(const CounterApp());
 }
